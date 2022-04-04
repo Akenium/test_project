afb606b11dce1b6f25611530cba30c7a2842408a
G.Reboud <geoffray.reboud@gmail.com>
2022-04-04 16:51:30 +0200

    test 2

--- s1.R
+++ s1.R
@@ -19,7 +19,7 @@ prior_brms <- c(prior(normal(0, 1), class = "Intercept"),
 
 
 
-glmm_S4 <- lmer(data = rd[which(rd$Strata.Sylv==4), ], 
+glmm_S4 <- lmer(data = rd[which(rd$Strata.Sylv==1), ], 
                 gig_abs ~ dbh + NCI + alt + WES + LES + dbh:WES + dbh:LES + NCI:WES + NCI:LES + Slope:WES + Slope:LES + Curvature:WES + Curvature:LES + alt:WES + alt:LES + SW:WES + SW:LES + (1 + alt + dbh + NCI | SpCode) + (1 | treeid) + (1 | year))
 
 glmm_S4
