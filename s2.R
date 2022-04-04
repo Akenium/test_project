31730d509c659e03bceb114abc3dee421d3bbc06
G.Reboud <geoffray.reboud@gmail.com>
2022-04-04 17:10:47 +0200

    brms NCI fonctionnel par strate

--- s2.R
+++ s2.R
@@ -18,10 +18,20 @@ prior_brms <- c(prior(normal(0, 1), class = "Intercept"),
 
 
 
+fit_brm_S2_NCIf <- brm(formula = gig_abs ~ 
+                         dbh + NCIf + alt  # level-1 model covariates
+                       + WES + LES + dbh:WES + dbh:LES + NCIf:WES + NCIf:LES + Slope:WES + Slope:LES + Curvature:WES + Curvature:LES + alt:WES + alt:LES + SW:WES + SW:LES  # trait effects on intercept and model covariates
+                       + (1 + alt + dbh + NCIf | SpCode) # random effects of species
+                       + (1 | treeid) + (1 | year)
+                       # + (1 | site)
+                       , #  random effects of individual, site (quadrat if within a plot or plot if comparison between plots), census
+                       data=rd[which(rd$Strata.Sylv==2), ], # standardized inout data 
+                       family=gaussian(), 
+                       prior=prior_brms, 
+                       iter=1200, chains=4, cores=4, seed=42, control=list(adapt_delta=0.99, max_treedepth=15))
 
-glmm_S4 <- lmer(data = rd[which(rd$Strata.Sylv==2), ], 
-                gig_abs ~ dbh + NCI + alt + WES + LES + dbh:WES + dbh:LES + NCI:WES + NCI:LES + Slope:WES + Slope:LES + Curvature:WES + Curvature:LES + alt:WES + alt:LES + SW:WES + SW:LES + (1 + alt + dbh + NCI | SpCode) + (1 | treeid) + (1 | year))
 
-glmm_S4
+save(fit_brm_S2_NCIf, file="fit_brm_S2_NCIf.RData")
+
 
 
