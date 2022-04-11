fd837c965ed804953257e142a450a33b8726795b
G.Reboud <geoffray.reboud@gmail.com>
2022-04-11 15:29:20 +0200

    scripts pour modèles avec NCI fonctionnel, spatial, par individu, et corrigés par aire

---
+++ NCIfonct_area_s2.R
@@ -0,0 +1,37 @@
+
+library(lme4)
+
+library(plyr); library(dplyr); library(tidyr)
+library(brms)
+options(mc.cores = parallel::detectCores())
+
+
+load(file = "rd.RData")
+
+
+# define priors for brm
+prior_brms <- c(prior(normal(0, 1), class = "Intercept"), 
+                prior(normal(0, 1), class = "b"),
+                prior(normal(0, 1), class = "sd"), 
+                prior(lkj(2), class = "cor"))
+
+
+
+
+fit_brm_S2_NCIfonct_area <- brm(formula = gig_abs ~ 
+                         dbh + NCIfonct_area + alt  # level-1 model covariates
+                       + WES + LES + dbh:WES + dbh:LES + NCIfonct_area:WES + NCIfonct_area:LES + Slope:WES + Slope:LES + Curvature:WES + Curvature:LES + alt:WES + alt:LES + SW:WES + SW:LES  # trait effects on intercept and model covariates
+                       + (1 + alt + dbh + NCIfonct_area | SpCode) # random effects of species
+                       + (1 | treeid) + (1 | year)
+                       # + (1 | site)
+                       , #  random effects of individual, site (quadrat if within a plot or plot if comparison between plots), census
+                       data=rd[which(rd$Strata.Sylv==2), ], # standardized inout data 
+                       family=gaussian(), 
+                       prior=prior_brms, 
+                       iter=1200, chains=4, cores=4, seed=42, control=list(adapt_delta=0.99, max_treedepth=15))
+
+
+save(fit_brm_S2_NCIfonct_area, file="fit_brm_S2_NCIfonct_area.RData")
+
+
+
---
+++ NCIspatial_s2.R
@@ -0,0 +1,37 @@
+
+library(lme4)
+
+library(plyr); library(dplyr); library(tidyr)
+library(brms)
+options(mc.cores = parallel::detectCores())
+
+
+load(file = "rd.RData")
+
+
+# define priors for brm
+prior_brms <- c(prior(normal(0, 1), class = "Intercept"), 
+                prior(normal(0, 1), class = "b"),
+                prior(normal(0, 1), class = "sd"), 
+                prior(lkj(2), class = "cor"))
+
+
+
+
+fit_brm_S2_NCIspatial <- brm(formula = gig_abs ~ 
+                         dbh + NCIspatial + alt  # level-1 model covariates
+                       + WES + LES + dbh:WES + dbh:LES + NCIspatial:WES + NCIspatial:LES + Slope:WES + Slope:LES + Curvature:WES + Curvature:LES + alt:WES + alt:LES + SW:WES + SW:LES  # trait effects on intercept and model covariates
+                       + (1 + alt + dbh + NCIspatial | SpCode) # random effects of species
+                       + (1 | treeid) + (1 | year)
+                       # + (1 | site)
+                       , #  random effects of individual, site (quadrat if within a plot or plot if comparison between plots), census
+                       data=rd[which(rd$Strata.Sylv==2), ], # standardized inout data 
+                       family=gaussian(), 
+                       prior=prior_brms, 
+                       iter=1200, chains=4, cores=4, seed=42, control=list(adapt_delta=0.99, max_treedepth=15))
+
+
+save(fit_brm_S2_NCIspatial, file="fit_brm_S2_NCIspatial.RData")
+
+
+
